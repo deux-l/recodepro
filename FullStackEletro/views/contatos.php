ce101f1c8a05c4f87614be05a9b23ebfe86c280c
deux-l <deuxl@outlook.com>
2020-11-03 15:57:10 -0300

    v1SiteHTML

---
+++ FullStackEletro/views/contatos.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/main.css"> 
+    <title>Contato - Full Stack Eletro</title>
+</head>
+
+<body>
+    <!--Menu-->
+    <table class="menu">
+        <tr class="cor-font">
+            <td><a href="../index.php"><img src="../imagens/logo.jpg" alt="logo-fullStackEletro"></a></td> 
+            <td><a href="produtos.php" title="Conheça nossos produtos!">Produtos</a></td> 
+            <td><a href="loja.php">Nossas lojas</a></td> 
+            <td><a href="contatos.php">Contato</a></td>   
+        </tr>
+    </table>
+    <!--Fim menu-->
+    <h2>Contato</h2>
+    <hr>
+<div>
+    <table align="center" class="contatos">
+        <tr>
+            <td>
+                <img src="../imagens/email.jpeg" alt="email">
+                <p>fullstackeletro@loja.com</p>
+            </td>
+            <td>
+                <img src="../imagens/zap.jpeg" alt="whatsapp">
+                <p>(11) 55555-3333</p>
+            </td>
+        </tr>
+    </table>
+
+    <form action="">
+        <h4>Nos envie uma mensagem, elogio ou reclamação</h4>
+        <h5>Nome: </h5>
+        <input type="text" size="30">
+        <h5>Mensagem: </h5>
+        <textarea marows="10" cols="50" maxlength="500"></textarea>
+
+        <input type="submit" value="Enviar">
+    </form>
+
+    <br> <br> <br> <br> <br>
+</div>
+  
+
+    <h4>Formas de pagamento</h4>
+    <img class="pag-img" src="../imagens/forma de pag-logos-cartoes.jpg" alt="Formas de pagamento">
+
+    <center><font face="Times" size=3 > Carlos &copy; RecodePro 2020</font></center>
+</body>
+
+</html>

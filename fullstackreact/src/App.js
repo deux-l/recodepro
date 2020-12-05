542b2d587e192ee43d08ef053d9b12fe731832f3
deux-l <deuxl@outlook.com>
2020-12-05 09:24:17 -0300

    projetoreact

---
+++ fullstackreact/src/App.js
@@ -0,0 +1,15 @@
+import HeaderNav from './Components/Pages/HeaderNav'
+import Footer from './Components/Pages/Footer'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <HeaderNav/>
+
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstackreact/src/Components/Pages/Footer.jsx
@@ -0,0 +1,11 @@
+import FormasDePagamento from '../img/FormasDePagamento.jpg'
+
+export default function Footer() {
+    return (
+        <>
+            <h4>Formas de pagamento</h4>
+            <img src={FormasDePagamento} alt="Formas de pagamento"/>
+            <p>Carlos &copy; RecodePro 2020</p>
+        </>
+    );
+}
